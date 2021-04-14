567ef4aa37d5a921881956ce9075320b07d4178e
ginzason <dark602@gmail.com>
2021-04-15 01:58:11 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,59 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  mode: 'universal',
+  head: {
+    
+    title: 'quantk',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+  telemetry: false,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/style-resources'
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    extractCSS: {
+      ignoreOrder: true
+    },
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {},
+  },
+  robots: {
+    UserAgent: '*',
+    Disallow: '/'
+  },
+  // env: {
+  //   API_BASE_URL:
+  //     process.env.NODE_ENV === 'development'
+  //       ? 'https://admin.api.staypia.com/dev'
+  //       : 'https://admin.api.staypia.com/prod',
+  // },
+}
